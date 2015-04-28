3248c3daf0dea63b39d2845eacc2a79edcd9acaf
raulgomezn <raul.gomezn@hotmail.com>
2015-04-27 19:07:15 -0500

    Ajustes

--- ArandaLogica/LogicaGeneral.cs
+++ ArandaLogica/LogicaGeneral.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ----------------------------------------------------------------------
+// <copyright file="LogicaGeneral.cs" company="Personal">
+// Todos los derechos reservados.
+// Autor: Raul Gomez
+// Fecha: Abril 2015
+// </copyright>
+// <summary>Clase LogicaGeneral.</summary>
+// ----------------------------------------------------------------------
 
 namespace ArandaLogica
 {
+    #region
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Acceso;
+    #endregion
+
     public class LogicaGeneral
     {
+        /// <summary>
+        /// Listar Permisos.
+        /// </summary>
+        /// <returns>Retorna la lista de los permisos con nombre y llave.</returns>
+        public IList<String> ListarPermisos() 
+        { 
+             
+            dbarandaEntities context = new dbarandaEntities();
+            
+                    List<string> conditions = new List<string>();
+
+                    var query = from es in context.permiso.AsNoTracking()
+                                select new { es.idPermiso, es.Nombre };
+                    query = query.Distinct();
+            return conditions;
+        }
     }
 }
